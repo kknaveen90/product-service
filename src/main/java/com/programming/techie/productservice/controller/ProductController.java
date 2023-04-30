e5d7bfe5da09fdcbeee976f2420d8b26ab13db44
kknaveen90 <36166060+kknaveen90@users.noreply.github.com>
2023-04-30 20:07:59 +0530

    src

---
+++ src/main/java/com/programming/techie/productservice/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.programming.techie.productservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final String hello = "Hello Naveen Sir";
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String helloex() {
+        return hello;
+    }
+   /* private final ProductRepository productRepository;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody Product product) {
+        productRepository.save(product);
+    }*/
+}
